7b195fa9bbe12960493ca7111e3c3c19f40d0423
Vitaly Pokrovsky <vitalik_pokrovsky@mail.ru>
2023-10-25 20:01:15 +0700

    AP finish

--- AdminPanel.xaml.cs
+++ AdminPanel.xaml.cs
@@ -11,3 +11,89 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+
+namespace rsad
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminPanel.xaml
+    /// </summary>
+    public partial class AdminPanel : Window
+    {
+
+        public AdminPanel()
+        {
+            InitializeComponent();
+            if(DataClass.myRole == "Сотрудник")
+            {
+                stafff.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                stafff.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void RequestBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RequestWindow requestWindow = new RequestWindow();
+            requestWindow.Show();
+            this.Close();
+        }
+
+        private void ClientsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ClientsWindow clients = new ClientsWindow();
+                clients.Show();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void StaffBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                StaffMainWindow mainWindow = new StaffMainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void LkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LkWindow mainWindow = new LkWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
