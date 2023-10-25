77b2e37870850599e55baeae2af23bd4c2c81cef
Vitaly Pokrovsky <vitalik_pokrovsky@mail.ru>
2023-10-25 19:44:06 +0700

    Using libraries and some code

--- WordHelper.cs
+++ WordHelper.cs
@@ -6,3 +6,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Markup;
 using Word = Microsoft.Office.Interop.Word;
+
+namespace rsad
+{
+    internal class WordHelper
+    {
+        private FileInfo _fileInfo;
+
+        public WordHelper(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                _fileInfo = new FileInfo(fileName);
+            }
+            else
+            {
+                throw new ArgumentException("File not found");
+            }
+        }
+    }
+}
